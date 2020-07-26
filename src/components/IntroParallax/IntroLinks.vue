a52a314558ac8ff0ef4919a29032c84cea897017
yashdhume <yash_dhume@yahoo.ca>
2020-07-26 16:33:07 -0400

    Extracted Icons with tooltips to its own component

--- src/components/IntroParallax/IntroLinks.vue
+++ src/components/IntroParallax/IntroLinks.vue
@@ -1,35 +1,19 @@
 <template>
     <v-column>
         <v-column v-for="i in socialLinks" :key="i.title">
-            <v-hover v-slot:default="{ hover }">
-                <v-btn x-large dark icon>
-                    <v-tooltip bottom>
-                        <template v-slot:activator="{ on }">
-                            <a :href="i.link" style="text-decoration:none">
-                                <v-icon x-large v-on="on"
-                                        :color=changeColor(hover)>
-                                    {{i.icon}}
-                                </v-icon>
-                            </a>
-                        </template>
-                        <span>{{i.title}}</span></v-tooltip>
-                </v-btn>
-            </v-hover>
+            <IconsWithTooltip :title="i.title" :icon="i.icon" color="red" :link="i.link" baseColor="white"/>
         </v-column>
     </v-column>
 </template>
 
 <script>
+    import IconsWithTooltip from "../miscellaneous/IconsWithTooltip";
     export default {
         name: "IntroLinks",
+        components: {IconsWithTooltip},
         props: {
             socialLinks:Array,
         },
-        methods:{
-            changeColor(hover) {
-                return hover ? "red" : "white"
-            },
-        }
     }
 </script>
 
---
+++ src/components/miscellaneous/IconsWithTooltip.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-hover v-slot:default="{ hover }">
+        <v-btn x-large dark icon>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <a :href="link" style="text-decoration:none">
+                        <v-icon x-large v-on="on"
+                                :color=changeColor(hover)>
+                            {{icon}}
+                        </v-icon>
+                    </a>
+                </template>
+                <span>{{title}}</span></v-tooltip>
+        </v-btn>
+    </v-hover>
+</template>
+
+<script>
+    export default {
+        name: "IconsWithTooltip",
+        props:{
+            icon: String,
+            title: String,
+            link: String,
+            color: String,
+            baseColor: String,
+        },
+        methods:{
+            changeColor(hover) {
+                return hover ? this.color : this.baseColor
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
